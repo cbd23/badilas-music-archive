e0c5beb96707ad5c85a52f1f076c8dd7c461ca16
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-10-31 15:28:09 +0200

    Assign routers & config app for serving static assets.

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import { fileURLToPath } from 'url'
 import path from "path"
 
 // import routers
-
+import indexRouter from "./routes/indexRouter.js"
+import artistsRouter from "./routes/artistsRouter.js"
+import albumsRouter from "./routes/albumsRouter.js"
+import genresRouter from "./routes/genresRouter.js"
+import labelsRouter from "./routes/labelsRouter.js"
 
 // define __filename & __dirname for ejs setup using ESM
 const __filename = fileURLToPath(import.meta.url)
@@ -16,10 +20,20 @@ const app = express()
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+// config app for serving static assets (like CSS files)
+const assetsPath = path.join(__dirname, "public")
+app.use(express.static(assetsPath))
+
 // use urlencoded for POST requests
 app.use(express.urlencoded({ extended: true }))
 
 // assign routers
+app.use("/artists", artistsRouter)
+app.use("/albums", albumsRouter)
+app.use("/genres", genresRouter)
+app.use("/labels", labelsRouter)
+app.use("/", indexRouter)
+
 
 
 const PORT = 3000
