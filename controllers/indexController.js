9dd72c6d55bc9033112fccb4fa6c8d3d7b8be9f5
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-11-14 18:50:03 +0200

    Get all artists inside 'Trending artists' section (to be modified later).

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,10 @@ async function indexGet(req, res) {
     // fetch the newest 10 albums to be displayed inside 'New Releases' section
     const newReleases = await db.getNewReleases()
 
-    res.render("index", { newReleases: newReleases })
+    // fetch the artists
+    const artists = await db.getAllArtists()
+
+    res.render("index", { newReleases: newReleases, artists: artists })
 }
 
 export const indexController = {
