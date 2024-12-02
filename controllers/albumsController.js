2a4f1908d6226f277b6142bc2b4af73a2d96ca56
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-02 21:20:30 +0200

    Add albumGet fn.

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -6,7 +6,17 @@ async function albumsGet(req, res) {
     res.render("albums", { albums: albums })
 }
 
+async function albumGet(req, res) {
+
+    const id = req.params.albumId
+
+    // fetch info based on album's id
+    const album = await db.getAlbum(id)
+
+    res.render("album", { album: album[0] })
+}
+
 export const albumsController = {
     albumsGet,
-    
+    albumGet,
 }
