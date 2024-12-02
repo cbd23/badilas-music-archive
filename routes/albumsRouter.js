3e820e205e5b00204ab3afd46c0ce7a6d0afdf0a
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-02 21:24:20 +0200

    Handle GET req on /:albumId.

--- routes/albumsRouter.js
+++ routes/albumsRouter.js
@@ -3,6 +3,8 @@ import { albumsController } from "../controllers/albumsController.js"
 
 const albumsRouter = Router()
 
+albumsRouter.get("/:albumId", albumsController.albumGet)
+
 albumsRouter.get("/", albumsController.albumsGet)
 
 export default albumsRouter
