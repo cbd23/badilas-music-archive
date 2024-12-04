64cb109646151d8cd3492086717afa7579971f55
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-04 15:59:00 +0200

    Add a function that gets all albums having a certain genre and updates the view for /genres/:genreId.

--- controllers/genresController.js
+++ controllers/genresController.js
@@ -6,7 +6,16 @@ async function genresGet(req, res) {
     res.render("genres", { genres: genres })
 }
 
+async function albumsHavingGenreGet(req, res) {
+
+    const id = req.params.genre
+
+    const albumsHavingGenre = await db.getAlbumsHavingGenre(id)
+
+    res.render("albumsHavingGenre", { albumsHavingGenre: albumsHavingGenre })
+}
+
 export const genresController = {
     genresGet,
-
+    albumsHavingGenreGet,
 }
