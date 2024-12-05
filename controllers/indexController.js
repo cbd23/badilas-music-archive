0333858a24c714e9726120c670fbec8bc5e5b942
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-05 11:48:05 +0200

    Rm comments.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,8 @@
 import db from "../db/queries.js"
 
 async function indexGet(req, res) {
-    // fetch the newest 10 albums to be displayed inside 'New Releases' section
     const newReleases = await db.getNewReleases()
-
-    // fetch the trending artists
     const artists = await db.getTrendingArtists()
-
     res.render("index", { newReleases: newReleases, artists: artists })
 }
 
