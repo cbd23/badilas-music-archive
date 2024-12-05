780f399475453210a7225eec6022b87339dcc470
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-05 11:45:49 +0200

    Rm comment.

--- controllers/artistsController.js
+++ controllers/artistsController.js
@@ -1,13 +1,10 @@
 import db from "../db/queries.js"
 
 async function artistsGet(req, res) {
-    // fetch all artists
     const artists = await db.getAllArtistsAsc()
-
     res.render("artists", { artists: artists })
 }
 
 export const artistsController = {
     artistsGet,
-
 }
