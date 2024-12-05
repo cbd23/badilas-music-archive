c4e0d92c184184586b5ea4ae8a76ce3746c04126
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-05 11:44:21 +0200

    Add fn that gets all albums released by a specific label.

--- controllers/labelsController.js
+++ controllers/labelsController.js
@@ -2,18 +2,13 @@ import db from "../db/queries.js"
 
 async function labelsGet(req, res) {
     const labels = await db.getAllLabels()
-
     res.render("labels", { labels: labels })
 }
 
 async function albumsHavingLabelGet(req, res) {
-
     const id = req.params.labelId
-
     const albumsHavingLabel = await db.getAlbumsHavingLabel(id)
-
     const labelName = albumsHavingLabel.length > 0 ? "Albums released by " + albumsHavingLabel[0].name : "Sorry, no albums found from the selected label."
-
     res.render("albumsHavingLabel", { albumsHavingLabel: albumsHavingLabel, labelName: labelName })
 }
 
