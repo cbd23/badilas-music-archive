02a0410096378f4335ad180b7442de37a5d9c298
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-10 22:26:45 +0200

    Handle /artists/:artistId

--- routes/artistsRouter.js
+++ routes/artistsRouter.js
@@ -3,6 +3,7 @@ import { artistsController } from "../controllers/artistsController.js"
 
 const artistsRouter = Router()
 
+artistsRouter.get("/:artistId", artistsController.artistGet)
 artistsRouter.get("/", artistsController.artistsGet)
 
 export default artistsRouter
