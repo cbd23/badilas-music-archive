f77617db3cbac8bd888171ab8a1ff69cc402b1c7
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-11 15:31:49 +0200

    Add a fn that removes spaces from artists' names and replaces them with underscores.

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,4 +1,9 @@
 export function formatTitleForWiki(title) {
-    // Remove special characters and replace spaces with underscores
-    return title.replace(/[^a-zA-Z0-9\s]/g, "").replace(/\s+/g, "_");
+    // remove special characters and replace spaces with underscores
+    return title.replace(/[^a-zA-Z0-9\s]/g, "").replace(/\s+/g, "_")
+}
+
+export function formatNameForWiki(name) {
+    // replace spaces with underscores
+    return name.replace(/\s+/g, "_")
 }
