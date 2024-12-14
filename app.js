e556c4aa5bd6c573894158ae462c4fdda1a86a58
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-12-14 17:37:14 +0200

    Specify the host on which the app will listen.

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use("/about", aboutRouter)
 app.use("/", indexRouter)
 
 
-const PORT = 3000
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`server is running on PORT:${PORT}`)
 })
